4fcafa35264a50657733b2d5b42c544395abb936
Tabioka <donovanbrady102@gmail.com>
2025-02-10 03:49:03 -0500

    Update init.lua

--- modules/game_framework/init.lua
+++ modules/game_framework/init.lua
@@ -1,1 +1,19 @@
+-- Game Framework Initialization
+local GameFramework = {}
+GameFramework.config = require(game.ReplicatedStorage.Config)
+
+-- Initializing core systems
+function GameFramework:Init()
+    print("Initializing Game Framework...")
+    self:InitSystems()
+end
+
+-- System Initialization Logic
+function GameFramework:InitSystems()
+    require(game.ReplicatedStorage.GameLoop):Start()
+    require(game.ReplicatedStorage.DataPersistence):LoadData()
+    require(game.ReplicatedStorage.EventHandling):InitializeEvents()
+end
+
+return GameFramework
 
